a7a5f6b67fa87355da9ec53ddf6f1a9d15c4df00
duparc <merryduparc@gmail.com>
2025-09-14 15:45:19 +0200

    Add SO SAT noise and moved Nls plots

--- compute_Nls.py
+++ compute_Nls.py
@@ -18,11 +18,12 @@ from biref_forecasts import utils
 SAVE_PATH = 'data/Nls'
 os.makedirs(SAVE_PATH, exist_ok=True)
 
-EXPERIMENTS = ['Planck', 'SO LAT']
+EXPERIMENTS = ['Planck', 'SO LAT', 'SO SAT']
 
 BANDS = {
     'Planck': ['030', '044', '070', '100', '143', '217', '353'],
     'SO LAT': ['027', '039', '093', '145', '225', '280'],
+    'SO SAT': ['027', '039', '093', '145', '225', '280'],
 }
 
 # RMS polarization noise in uK.deg
@@ -37,12 +38,20 @@ RMS = {
         '353': 7.31,
     },
     'SO LAT': {         # SO LAT numbers are in uK.arcmin
-        '027': 71 / 60,
-        '039': 36 / 60,
-        '093': 8.0 / 60,
-        '145': 10 / 60,
-        '225': 22 / 60,
-        '280': 54 / 60,
+        '027': 61 / 60 * np.sqrt(2),
+        '039': 30 / 60 * np.sqrt(2),
+        '093': 5.3 / 60 * np.sqrt(2),
+        '145': 6.6 / 60 * np.sqrt(2),
+        '225': 15 / 60 * np.sqrt(2),
+        '280': 35 / 60 * np.sqrt(2),
+    },
+    'SO SAT': {         # SO LAT numbers are in uK.arcmin
+        '027': 35 / 60 * np.sqrt(2),
+        '039': 21 / 60 * np.sqrt(2),
+        '093': 2.6 / 60 * np.sqrt(2),
+        '145': 3.3 / 60 * np.sqrt(2),
+        '225': 6.3 / 60 * np.sqrt(2),
+        '280': 16 / 60 * np.sqrt(2),
     },
 }
 
@@ -65,6 +74,14 @@ FWHM = {
         '225': 1.0,
         '280': 0.9,
     },
+    'SO SAT': {
+        '027': 91,
+        '039': 63,
+        '093': 30,
+        '145': 17,
+        '225': 11,
+        '280': 9,
+    },
 }
 
 L_KNEE = {
@@ -76,8 +93,36 @@ L_KNEE = {
         '225': 700,
         '280': 700,
     },
+    'SO SAT': {
+        '027': 30,
+        '039': 30,
+        '093': 50,
+        '145': 50,
+        '225': 70,
+        '280': 100,
+    },
 }
 L_KNEE['Planck'] = {band: None for band in BANDS['Planck']}
+
+ALPHA_1fN = {
+    'SO LAT': {
+        '027': -1.4,
+        '039': -1.4,
+        '093': -1.4,
+        '145': -1.4,
+        '225': -1.4,
+        '280': -1.4,
+    },
+    'SO SAT': {
+        '027': -2.4,
+        '039': -2.4,
+        '093': -2.5,
+        '145': -3.0,
+        '225': -3.0,
+        '280': -3.0,
+    },
+}
+ALPHA_1fN['Planck'] = {band: None for band in BANDS['Planck']}
 
 
 SPECTRA_TO_COMPARE = {
@@ -85,9 +130,13 @@ SPECTRA_TO_COMPARE = {
         band: f'data/spectra/NPIPE_BIREF/Dls_{band}x{band}_noise.dat' for band in ['100', '143', '217', '353']
     },
     'SO LAT': {
+        band: f'data/noise_model/Nls_SO{band}.dat' for band in BANDS['SO LAT']
+    },
+    'SO SAT': {
         band: None for band in BANDS['SO LAT']
     },
 }
+
 SPECTRA_TO_COMPARE['Planck']['030'] = None
 SPECTRA_TO_COMPARE['Planck']['044'] = None
 SPECTRA_TO_COMPARE['Planck']['070'] = None
@@ -95,9 +144,25 @@ SPECTRA_TO_COMPARE['Planck']['070'] = None
 COLORS = {
     'Planck': {band: f'C{i}' for i, band in enumerate(BANDS["Planck"])},
     'SO LAT': {band: f'C{i}' for i, band in enumerate(BANDS["SO LAT"])},
+    'SO SAT': {band: f'C{i}' for i, band in enumerate(BANDS["SO SAT"])},
 }
 
-def compute_Nls(rms:float, fwhm:float, l_knee:int) -> dict[np.ndarray]:
+PLOTS_LIMS = {
+    'Planck': {
+        'xlims': (0, 3000),
+        'ylims': (1, 50000)
+    },
+    'SO LAT': {
+        'xlims': (0, 5000),
+        'ylims': (.03, 3000)
+    },
+    'SO SAT': {
+        'xlims': (0, 1500),
+        'ylims': (.0003, 1000),
+    },
+}
+
+def compute_Nls(rms:float, fwhm:float, l_knee:int, alpha:float=-1.4) -> dict[np.ndarray]:
     beam = hp.gauss_beam(np.deg2rad(fwhm / 60), lmax=10000)
     
     mean = {}
@@ -109,8 +174,6 @@ def compute_Nls(rms:float, fwhm:float, l_knee:int) -> dict[np.ndarray]:
             mean[spec] = np.full_like(ls, (rms ** 2) * ((np.pi / 180) ** 2), dtype=float) * fac
             if l_knee is not None:
                 # From SO forecasts paper 1808.07445 (eq. 1)
-                # TODO put alpha and N_red_ratio as args
-                alpha = -1.4
                 N_red_ratio = 1.
                 mean[spec] *= (1 + N_red_ratio * (ls / l_knee)**alpha) 
         else:
@@ -127,24 +190,23 @@ for exp in EXPERIMENTS:
         rms = RMS[exp][band]
         fwhm = FWHM[exp][band]
         l_knee = L_KNEE[exp][band]
-        ls, Nls = compute_Nls(rms, fwhm, l_knee)
+        alpha_1fN = ALPHA_1fN[exp][band]
+        ls, Nls = compute_Nls(rms, fwhm, l_knee, alpha_1fN)
         so_spectra.write_ps(f'{SAVE_PATH}/Nl_{exp}_{band}.dat', ls, Nls, type='Dl', spectra=utils.spectra_pspy)
-        ax.plot(ls, Nls['EE'], label=f'{exp}{band}', color=COLORS[exp][band])
+        ax.plot(ls, Nls['EE'], label=f'{exp} {band}', color=COLORS[exp][band])
         if SPECTRA_TO_COMPARE[exp][band] is not None:
             try:
                 ls_comp, Nls_comp = so_spectra.read_ps(SPECTRA_TO_COMPARE[exp][band], spectra=utils.spectra_pspy)
-                ax.plot(ls_comp, Nls_comp['EE'], label=f'{exp}{band}_ext', color=COLORS[exp][band], linewidth=1)
+                ax.plot(ls_comp, Nls_comp['EE'], label=f'{exp} {band}_ext', color=COLORS[exp][band], linewidth=1)
             except:
                 print(f'{SPECTRA_TO_COMPARE[exp][band]} not found.')
-    ax.set_xlim(0, 5000)
-    ax.set_ylim(.1, 5000000)
+    ax.set_xlim(*PLOTS_LIMS[exp]['xlims'])
+    ax.set_ylim(*PLOTS_LIMS[exp]['ylims'])
     ax.set_yscale('log')
     ax.legend()
     ax.set_xlabel(r'$\ell$', fontsize=16)
-    ax.set_ylabel(r'$N_\ell$', fontsize=16)
+    ax.set_ylabel(r'$\ell(\ell + 1) N_\ell / (2\pi) [\mu K^2]$', fontsize=16)
     ax.set_title(f'{exp}', fontsize=16)
     plt.savefig(f'plots/Nls_{exp}.png')
     
     plt.clf()
-
-
